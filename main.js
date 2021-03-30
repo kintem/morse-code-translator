861a4c8ed256482994502b2d91e037e4187b96c8
Kinte Matulyte <=>
2021-03-30 15:31:53 +0100

    english to morse code translation function

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const userInput = document.querySelector("#user-input");
+const translation = document.querySelector("#translation");
+const form = document.querySelector("form");
 
-let translation = "";
-let morseUnit = "";
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const translate = (input) =>{
-  let array = input.split("");
-  array.map(letter=>{
-    return 
+const morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+const translate = (input) => {
+  const inputArray = input.split("");
+
+  let morseArray = inputArray.map(letter=>{
+    let index = alphabet.indexOf(letter);
+    return morseCode[index];
   })
+
+  translation.innerHTML = `${input} in morse code is ${morseArray.join(" ")}`;
 }
+
+form.addEventListener("submit", (event) =>{
+  event.preventDefault();
+  translate(userInput.value);
+});
