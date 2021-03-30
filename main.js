a4c0a12852c80f5fd248191878e391ac7ad30429
Kinte Matulyte <=>
2021-03-30 17:02:56 +0100

    morse to english translation

--- main.js
+++ main.js
@@ -4,23 +4,42 @@ const form = document.querySelector("form");
 
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const morseLetters = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+const morseCode = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
 
-const morseNumbers = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." ];
+const translate = (userInput) => {
+  if ((/[a-zA-Z0-9]/).test(userInput)) {
+    const inputArray = userInput.split("");
 
-const translate = (input) => {
-  const inputArray = input.split("");
+    const morseArray = inputArray.map(input=>{
+      if ((/[0-9]/).test(input)) {
+        return morseCode[+input]
+      } else if ((/[a-zA-Z]/).test(input)) {
+        let index = alphabet.indexOf(input.toLowerCase());
+        return morseCode[index+10]
+      } else if ((/[ ]/).test(input)) {
+        return " / ";
+      }
+    })
 
-  const morseArray = inputArray.map(input=>{
-    if ((/[0-9]/).test(input)) {
-      return morseNumbers[+input]
-    } else if ((/[a-zA-Z]/).test(input)) {
-      let index = alphabet.indexOf(input.toLowerCase());
-      return morseLetters[index]
-    }
-  })
+    translation.innerHTML = `"${userInput}" in morse code: <span>${morseArray.join(" ")}</span>`;
 
-  translation.innerHTML = `${input} in morse code is ${morseArray.join(" ")}`;
+  } else if ((/[.-]/).test(userInput)) {
+    const inputArray = userInput.split(" ");
+
+    const englishArray = inputArray.map(input=>{
+      let index = morseCode.indexOf(input);
+      
+      if (alphabet[index-10]) {
+        return alphabet[index-10];
+      } else if (input === "/") {
+        return "/";
+      } else {
+        return index;
+      }
+    }); 
+
+    translation.innerHTML = `"${userInput}" in English: <span>${englishArray.join(" ")}</span>`;
+  }
 }
 
 form.addEventListener("submit", (event) =>{
