f080cc20f6296475da6fab2b23361d5bc8058819
Kinte Matulyte <=>
2021-03-30 15:56:26 +0100

    add morse number translation

--- main.js
+++ main.js
@@ -4,14 +4,20 @@ const form = document.querySelector("form");
 
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+const morseLetters = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+const morseNumbers = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." ];
 
 const translate = (input) => {
   const inputArray = input.split("");
 
-  let morseArray = inputArray.map(letter=>{
-    let index = alphabet.indexOf(letter);
-    return morseCode[index];
+  let morseArray = inputArray.map(input=>{
+    if ((/[0-9]/).test(input)) {
+      return morseNumbers[+input]
+    } else if ((/[a-zA-Z]/).test(input)) {
+      let index = alphabet.indexOf(input.toLowerCase());
+      return morseLetters[index]
+    }
   })
 
   translation.innerHTML = `${input} in morse code is ${morseArray.join(" ")}`;
