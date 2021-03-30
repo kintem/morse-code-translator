46ee8dc32e4fde6345749ff253bfaab0740599cb
Kinte Matulyte <=>
2021-03-30 14:54:51 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const userInput = document.querySelector("#user-input");
+
+let translation = "";
+let morseUnit = "";
+
+const translate = (input) =>{
+  let array = input.split("");
+  array.map(letter=>{
+    return 
+  })
+}
